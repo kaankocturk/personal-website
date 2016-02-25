f32dafe51b3530e90823de14e73b59847c1bd758
kaankocturk <kkocturk1@babson.edu>
2016-02-24 16:36:23 -0800

    comingup

---
+++ main.js
@@ -0,0 +1,14 @@
+// 'use strict';
+//
+// var app = angular.module('someApp', ['ui.router', 'ngStorage']);
+//
+// app.config(function($stateProvider, $urlRouterProvider) {
+//   $stateProvider
+//     .state('creative', { url: '/', templateUrl: '/partials/home/home.html', controller: 'homeCtrl' })
+//     .state('problemsolver', { url: '/login', templateUrl: '/partials/home/login.html', controller: 'loginregister' })
+//     .state('designer', { url: '/register', templateUrl: '/partials/home/register.html', controller: 'loginregister' })
+//     .state('coder', { url: '/createProfile', templateUrl: '/partials/createProfile.html', controller: 'create' })
+//     .state('writer', { url: '/profile', templateUrl: '/partials/profile.html', controller: 'profile' })
+//     .state('analyticalthinker', { url: '/users', templateUrl: '/partials/users.html', controller: 'users' })
+//   $urlRouterProvider.otherwise('/');
+// });
